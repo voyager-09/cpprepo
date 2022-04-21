a871b145f02e5dcf0be59b50cc529552839548d6
Ritik <ritikrathore336699@gmail.com>
2022-04-21 08:21:20 +0530

    initial commit

---
+++ index.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+    system("cls");
+    
+    int a=1;
+
+    //make a=5 to make it error free
+
+    // date = 21/04/2022      time=8:18am
+
+    if(a!=5)
+    {
+        cout<<"File has an error\n";
+    }
+    else
+    {
+        cout<<"File is OK\n";
+    }
+
+
+    return 0;
+}
